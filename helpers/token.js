129ea951d8fb8e364c7e35d71d6306c4aeb05e57
tinho1123 <tinhoefaela@gmail.com>
2022-07-05 11:04:31 -0300

    configuration createToken options

--- helpers/token.js
+++ helpers/token.js
@@ -1,7 +1,7 @@
 const json = require('jsonwebtoken');
 
 const createToken = async (body) => {
-    return await json.sign(body, process.env.KEY_JWT)
+    return await json.sign(body, process.env.KEY_JWT, { expiresIn: '24h', algorithm: 'HS256'})
 }
 
 module.exports = {
